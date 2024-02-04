4d4eea8897d4f280cad37418284728ef737572fd
Othi Raja <othiraja5677@gmail.com>
2024-02-04 21:43:46 +0530

    key changes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
-function downloadResume() {
-    // Replace the URL with your Firebase Storage URL
-    var fileUrl = 'https://firebasestorage.googleapis.com/v0/b/resume-store-750a4.appspot.com/o/Resume-Amjathkhan-Shiekusman.pdf?alt=media';
+// function downloadResume() {
+//     // Replace the URL with your Firebase Storage URL
+//     var fileUrl = 'https://firebasestorage.googleapis.com/v0/b/resume-store-750a4.appspot.com/o/Resume-Amjathkhan-Shiekusman.pdf?alt=media';
 
-    var a = document.createElement('a');
-    a.href = fileUrl;
-    a.download = 'Resume-Amjathkhan-Shiekusman.pdf';
+//     var a = document.createElement('a');
+//     a.href = fileUrl;
+//     a.download = 'Resume-Amjathkhan-Shiekusman.pdf';
 
-    // Append the anchor element to the body
-    document.body.appendChild(a);
+//     // Append the anchor element to the body
+//     document.body.appendChild(a);
 
-    // Trigger a click event on the anchor element
-    a.click();
+//     // Trigger a click event on the anchor element
+//     a.click();
 
-    // Remove the anchor element from the body
-    document.body.removeChild(a);
-  }
+//     // Remove the anchor element from the body
+//     document.body.removeChild(a);
+//   }
+
+const openLink = (url) => {
+  window.open(url, '_blank');
+};
+const shopLink = ()=>{
+  window.open('https://pickybest.in/','_blank')
+
+}
+
+const linkedinshopLink = ()=>{
+  window.open('https://pickybest.in/','_blank')
+
+}
